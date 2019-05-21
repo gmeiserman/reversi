0d71f27e032ce005548361fcdc7fcc95eb869073
Gillian Eiserman <>
2019-05-21 07:24:51 -0400

    Updated to enable chat functionality

--- public/js/main.js
+++ public/js/main.js
@@ -18,4 +18,53 @@ if ('undefined' == typeof username || !username)
 	username = 'Anon_'+Math.random();
 }
 
-$('#messages').append('<h4>'+username+'<h4>');
+var chatroom='RoomOne';
+
+
+/* Connect to the socket server */
+
+var socket = io.connect();
+
+
+socket.on('log', function(array){
+	console.log.apply(console, array);
+});
+
+socket.on('join_room_response',function(payload){
+        if(payload.result == 'fail'){
+                alert(payload.message);
+                return;
+        }
+        $('#messages').append('<p>New user joinded the room: '+payload.username+'</p>');
+});
+
+socket.on('send_,message_response',function(payload){
+        if(payload.result == 'fail'){
+                alert(payload.message);
+                return;
+        }
+        $('#messages').append('<p><b>'+payload.username+' says:</b> '+payload.message+'</p>');
+});
+
+
+
+function send_message(){
+	var payload={};
+	payload.room = chatroom;
+        payload.username=username;
+	payload.message=$('#send_message_holder').val();
+	console.log('***Client log message: \'send_message\'  payload: '+JSON.stringify(payload));
+	socket.emit('send_message'.payload);
+}
+
+
+$(function(){
+	var payload={};
+	payload.room = chatroom;
+	payload.username=username;
+
+	console.log('***Client log message: \'join_room\' payload: '+JSON.stringify(payload));
+	socket.emit('join_room',payload);
+});
+
+
